7b11569390867b4949d6fcbe5499a10b9d438ba1
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-07-08 16:27:53 -0500

    fin

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,14 +12,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject(FormBuilder)
+  private authService = inject( AuthService )
+  private router = inject( Router )
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['alex001@gmail.com', [Validators.required, Validators.email]],
+    password: ['123457', [Validators.required, Validators.minLength(6)]],
   })
 
   login() {
     console.log( this.myForm.value )
+    const { email, password } = this.myForm.value
+    this.authService.login( email, password )
+      .subscribe({
+        next: data => this.router.navigateByUrl('/dashboard'),
+        error: error => {
+          Swal.fire({
+            title: 'Error!',
+            text: error,
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          })
+          // console.log({ error })
+        },
+      })
   }
 
 }
